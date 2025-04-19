28f046d7f7cc8e4f09f53989231edb12f7a2e0b8
VSaiNagendra <vsainagendra.tech@gmail.com>
2025-04-19 20:12:16 +0530

    extract url path

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -9,7 +9,17 @@ public class Main {
       ServerSocket serverSocket = new ServerSocket(4221);
       serverSocket.setReuseAddress(true);
       Socket socket = serverSocket.accept();
-      socket.getOutputStream().write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
+      BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+      OutputStream out = socket.getOutputStream();
+
+      String requestLine = in.readLine();
+      String urlPath = requestLine.split(" ")[1];
+      if (urlPath.equals("/")) {
+        out.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
+      } else {
+        out.write("HTTP/1.1 404 Not Found\r\n\r\n".getBytes())
+      }
+
       System.out.println("accepted new connection");
       socket.close();
     } catch (IOException e) {
